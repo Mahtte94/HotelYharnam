d10fe5209a37b0665fc86bfa963e8066555fc850
Mahtte94 <mahtias.jebrand@live.se>
2024-12-19 19:13:49 +0100

    Added dates that you can book and price for each day

--- index.php
+++ index.php
@@ -2,20 +2,62 @@
 
 require __DIR__ . "/functions.php";
 
-$database = new PDO("sqlite:../sql/hotel.db");
+// $database = new PDO("sqlite: hotel.db");
 
 if (isset($_POST['transfer_code'])) {
   $transferCode = htmlspecialchars(trim($_POST['transfer_code']));
   $gunsCost =  isset($_POST['guns']) ? 2 : 0;
   $rifleCost = isset($_POST['rifle']) ? 3 : 0;
-  $totalCost = $gunsCost + $rifleCost;
+  $featureCost = $gunsCost + $rifleCost;
   $user = 'Mahtias';
+
+  $arrivalDate = $_POST['arrival_date'];
+  $departureDate = $_POST['departure_date'];
+
+
+  if (!$arrivalDate || !$departureDate) {
+    echo "Error: Both arrival and departure dates are required.";
+    exit;
+  }
+
+  if ($departureDate <= $arrivalDate) {
+    echo "Error: Departure date must be after arrival date.";
+    exit;
+  }
+
+  $start = new DateTime($arrivalDate);
+  $end = new DateTime($departureDate);
+  $totalDays = $start->diff($end)->days;
+
+  $roomType = $_POST['rooms'];
+  $roomCost = 0;
+
+  switch ($roomType) {
+    case 'economy':
+      $roomCost = 1 * $totalDays;
+      break;
+    case 'standard':
+      $roomCost = 2 * $totalDays;
+      break;
+    case 'luxury':
+      $roomCost = 4 * $totalDays;
+      break;
+    default:
+      $roomCost = 0;
+  }
+
+
+
+  $totalCost = $featureCost + $roomCost;
+  echo "Room Type Selected: " . $roomType . "<br>";
+  echo "Room Cost: " . $roomCost . "<br>";
+  echo "Feature Cost: " . $featureCost . "<br>";
+  echo "Total Cost: " . $totalCost . "<br>";
 
 
   if (!isValidUuid($transferCode)) {
 
-    echo "Not valid";
-
+    echo "Not valid transfercode or not enough balance";
   } else {
 
     $balance = sendTransferRequest($transferCode, $totalCost);
@@ -24,18 +66,13 @@ if (isset($_POST['transfer_code'])) {
 
       $deposit = depositTransfer($user, $transferCode);
 
-      echo "Deposit succesful:$deposit";
-
+      echo "Deposit succesful: $deposit";
     } else {
 
       echo "Not enough currency. Required: $totalCost";
-
     }
   }
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +100,16 @@ if (isset($_POST['transfer_code'])) {
         <option value="standard">Standard</option>
         <option value="luxury">Luxury</option>
       </select>
+
+      <label for="arrival_date">Arrival Date</label>
+      <input type="date" id="arrival_date" name="arrival_date" min="2025-01-10" max="2025-01-31" required>
+
+      <label for="departure_date">Departure Date</label>
+      <input type="date" id="departure_date" name="departure_date" min="2025-01-11" max="2025-01-31" required>
+
+
   </form>
+
 </body>
 
 </html>
